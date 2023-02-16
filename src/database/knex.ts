b49cda8eb222dd5d394597a2a40013e5122d4e6d
rafa-souza-dev <rafiradeveloper@gmail.com>
2023-02-16 15:46:52 -0300

    config: knex query builder implementation

---
+++ src/database/knex.ts
@@ -0,0 +1,15 @@
+import { knex as setupKnex, Knex } from 'knex'
+
+export const config: Knex.Config = {
+  client: 'sqlite',
+  connection: {
+    filename: 'app.db',
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './src/database/migrations',
+  },
+}
+
+export const knex = setupKnex(config)
