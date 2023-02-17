423b77ee92fbb8ac25b2dfad1e759e70c8c8e577
rafa-souza-dev <rafiradeveloper@gmail.com>
2023-02-16 23:41:52 -0300

    chore: added knex types

---
+++ src/@types/knex.d.ts
@@ -0,0 +1,12 @@
+import { Knex } from 'knex'
+
+declare module 'knex/types/tables' {
+  export interface Tables {
+    customers: {
+      id: string
+      name: string
+      cpf: string
+      birth_date: string
+    }
+  }
+}
