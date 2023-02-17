f9f7d13ad53d3c07112b2f75618bd29b7cb05a4f
rafa-souza-dev <rafiradeveloper@gmail.com>
2023-02-17 13:05:00 -0300

    feat: sqlite3 customers repository

---
+++ src/repositories/CustomersSqliteRepository.ts
@@ -0,0 +1,34 @@
+import { knex } from "../database/knex";
+import { Customer } from "../domain/Customer";
+import { ICustomersRepository } from "./ICustomersRepository";
+
+export class CustomersSqliteRepository implements ICustomersRepository {
+    public async save(customer: Customer): Promise<void> {
+        await knex('customers').insert({
+            id: customer.id,
+            cpf: customer.getCPF(),
+            name: customer.getName(),
+            birth_date: new Date(customer.getBirthDate())
+        })
+    }
+
+    public async findByCPF(cpf: string): Promise<Customer | null> {
+        const findCustomer = await knex('customers').select('*')
+            .where('cpf', cpf).first()
+
+        
+
+        if (findCustomer) {
+            const date = new Date(findCustomer.birth_date)
+
+            return new Customer(
+                findCustomer.name,
+                findCustomer.cpf,
+                date,
+                findCustomer.id
+            )
+        }
+
+        return null
+    }
+}
---
+++ src/repositories/ICustomersRepository.ts
@@ -0,0 +1,6 @@
+import { Customer } from "../domain/Customer";
+
+export interface ICustomersRepository {
+    save: (customer: Customer) => Promise<void>
+    findByCPF: (cpf: string) => Promise<Customer | null>
+}
