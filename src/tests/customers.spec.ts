869dfe1ee81e8385632374c066216ef35028a958
rafa-souza-dev <rafiradeveloper@gmail.com>
2023-02-17 18:58:14 -0300

    test: customers listing

--- src/tests/customers.spec.ts
+++ src/tests/customers.spec.ts
@@ -1,5 +1,5 @@
 import request from 'supertest'
-import { describe, it, beforeAll, afterAll, beforeEach } from 'vitest'
+import { describe, it, beforeAll, afterAll, beforeEach, expect } from 'vitest'
 import { execSync } from 'node:child_process'
 import { app } from '../app'
 
@@ -145,5 +145,76 @@ describe('customer search by cpf', () => {
         await request(app.server)
             .get('/customers/658.328.270-40')
             .expect(404)
+    })
+})
+
+describe('customers listing', () => {
+    it('should be possible to list the users using pagination args', async () => {
+        await request(app.server)
+            .post('/customers')
+            .send({
+                'name': 'Rafael Souza',
+                'cpf': '111.444.777-35',
+                'birth_date': '2002-06-22'
+            })
+
+        await request(app.server)
+            .post('/customers')
+            .send({
+                'name': 'Fulano de Tal',
+                'cpf': '622.096.840-99',
+                'birth_date': '1999-05-03'
+            })
+
+        const response = await request(app.server)
+            .get('/customers?page=1&limit=1')
+            .expect(200)
+        
+        expect(response.body.results.length).toEqual(1)
+    })
+
+    it('listing on a non-existent page should return 0 results', async () => {
+        await request(app.server)
+            .post('/customers')
+            .send({
+                'name': 'Rafael Souza',
+                'cpf': '111.444.777-35',
+                'birth_date': '2002-06-22'
+            })
+
+        await request(app.server)
+            .post('/customers')
+            .send({
+                'name': 'Fulano de Tal',
+                'cpf': '622.096.840-99',
+                'birth_date': '1999-05-03'
+            })
+
+        const response = await request(app.server)
+            .get('/customers?page=50&limit=10')
+
+        expect(response.body.results.length).toEqual(0)
+    })
+
+    it('should not be possible to use parameters with negative value or letters', async () => {
+        await request(app.server)
+            .post('/customers')
+            .send({
+                'name': 'Rafael Souza',
+                'cpf': '111.444.777-35',
+                'birth_date': '2002-06-22'
+            })
+
+        await request(app.server)
+            .post('/customers')
+            .send({
+                'name': 'Fulano de Tal',
+                'cpf': '622.096.840-99',
+                'birth_date': '1999-05-03'
+            })
+
+        await request(app.server)
+            .get('/customers?page=-1&limit=a')
+            .expect(422)
     })
 })
