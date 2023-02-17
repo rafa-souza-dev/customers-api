92f6a22c306638edab6b300c267bc96639daf085
rafa-souza-dev <rafiradeveloper@gmail.com>
2023-02-17 18:20:54 -0300

    chore: supertest setup

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,4 +1,10 @@
-import 'dotenv/config'
+import { config } from 'dotenv'
 import { envSchema } from '../validation/env-schemas'
+
+if (process.env.NODE_ENV === 'test') {
+    config({ path: '.env.test' })
+} else {
+    config()
+}
 
 export const env = envSchema.parse(process.env)
--- src/validation/env-schemas.ts
+++ src/validation/env-schemas.ts
@@ -3,5 +3,5 @@ import { z } from "zod";
 export const envSchema = z.object({
     NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
     DATABASE_URL: z.string().default("app.db"),
-    PORT: z.number().default(8000)
+    PORT: z.string().transform(port => Number(port)).default('8000')
 })
