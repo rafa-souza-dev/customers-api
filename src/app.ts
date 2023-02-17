69e99c7b8e9b3ed0e6a48017ac1d5cf5e5fdff90
rafa-souza-dev <rafiradeveloper@gmail.com>
2023-02-17 19:44:43 -0300

    feat: enabled logs

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,15 @@ import fastify from "fastify";
 import { env } from "./env";
 import { customersRoutes } from "./routes/customers";
 
-export const app = fastify()
+const envToLogger = {
+    development: true,
+    production: true,
+    test: false,
+}
+
+export const app = fastify({
+    logger: envToLogger[env.NODE_ENV] ?? true
+})
 
 app.register(customersRoutes, {
     prefix: '/customers'
