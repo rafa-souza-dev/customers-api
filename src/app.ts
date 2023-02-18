73eb3b9aaedc644046dd68f3d234e4cdb899af67
rafa-souza-dev <rafiradeveloper@gmail.com>
2023-02-18 12:57:12 -0300

    feat: setup swagger-ui docs

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import fastify from "fastify";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 import { env } from "./env";
 import { customersRoutes } from "./routes/customers";
 
@@ -11,6 +13,10 @@ const envToLogger = {
 export const app = fastify({
     logger: envToLogger[env.NODE_ENV] ?? true
 })
+
+app.register(fastifySwagger)
+
+app.register(fastifySwaggerUi)
 
 app.register(customersRoutes, {
     prefix: '/customers'
