e572039183ca095c721b6ba9911f7701ab189e1c
rafa-souza-dev <rafiradeveloper@gmail.com>
2023-02-18 14:51:39 -0300

    chore: created  customersGetSwaggerSchema

---
+++ src/docs/swagger/customers-get-swagger-schema.ts
@@ -0,0 +1,62 @@
+export const customersGetSwaggerSchema = {
+    schema: {
+        description: 'get customer by cpf',
+        tags: ['customer'],
+        query: {
+            type: 'object',
+            properties: {
+              page: { type: 'string' },
+              limit: { type: 'string' },
+            }
+        },
+        response: {
+          200: {
+            description: 'Success response',
+            type: 'object',
+            properties: {
+              count: { type: 'number' },
+              page: { type: 'number' },
+              limit: { type: 'number' },
+              previous: { type: ['null', 'string'] },
+              next: { type: ['null', 'string'] },
+              results: {
+                type: 'array',
+                items: {
+                  type: 'object',
+                  properties: {
+                    id: { type: 'string' },
+                    name: { type: 'string' },
+                    cpf: { type: 'string' },
+                    birth_date: { type: 'string' }
+                  }
+                }
+              },
+            }
+          },
+          422: {
+            description: 'Query params validation error',
+            type: 'object',
+            properties: {
+              errors: {
+                type: 'array',
+                items: {
+                  type: 'object',
+                  properties: {
+                    code: { type: "string" },
+                    expected: { type: "string" },
+                    received: { type: "string" },
+                    path: {
+                      type: 'array',
+                      items: {
+                        type: "string"
+                      }
+                    },
+                    message: { type: "string" }
+                  }
+                },
+              }
+            }
+          },
+        },
+    }
+}
--- src/routes/customers.ts
+++ src/routes/customers.ts
@@ -2,13 +2,14 @@ import { FastifyInstance } from "fastify";
 import { createCustomerController, findAndCountAllCustomersController, findCustomerByCPFController } from "../use-cases";
 import { customersPostSwaggerSchema } from '../docs/swagger/customers-post-swagger-schema'
 import { customersGetByCPFSwaggerSchema } from "../docs/swagger/customers-get-by-cpf-swagger-schema";
+import { customersGetSwaggerSchema } from "../docs/swagger/customers-get-swagger-schema";
 
 export async function customersRoutes(app: FastifyInstance) {
     app.post('/', customersPostSwaggerSchema, async (req, res) => {
         return createCustomerController.handle(req, res)
     })
 
-    app.get('/', async (req, res) => {
+    app.get('/', customersGetSwaggerSchema, async (req, res) => {
         return findAndCountAllCustomersController.handle(req, res)
     })
 
