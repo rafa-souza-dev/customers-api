2e5643ee45c56a04df0c696461976171664778a3
rafa-souza-dev <rafiradeveloper@gmail.com>
2023-02-18 14:57:17 -0300

    chore: update swagger description

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,23 @@ export const app = fastify({
     logger: envToLogger[env.NODE_ENV] ?? true
 })
 
-app.register(fastifySwagger)
+app.register(fastifySwagger, {
+    swagger: {
+        info: {
+          title: 'Customers API',
+          description: 'Customer api documentation with swagger',
+          version: '1.0.0'
+        },
+        externalDocs: {
+          url: 'https://swagger.io',
+          description: 'Find more info here'
+        },
+        host: 'localhost',
+        schemes: ['http'],
+        consumes: ['application/json'],
+        produces: ['application/json'],
+    }
+})
 
 app.register(fastifySwaggerUi)
 
