f41d4a62cced9d5c1aebdaa96c65908b6f0d714c
rafa-souza-dev <rafiradeveloper@gmail.com>
2023-02-17 21:18:44 -0300

    chore: added HOST variable

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,8 @@ import { app } from "./app"
 import { env } from "./env"
 
 app.listen({
-    port: env.PORT
+    host: env.HOST,
+    port: env.PORT,
 }).then(() => {
     console.log("HTTP Server running...")
 })
--- src/validation/env-schemas.ts
+++ src/validation/env-schemas.ts
@@ -3,5 +3,6 @@ import { z } from "zod";
 export const envSchema = z.object({
     NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
     DATABASE_URL: z.string().default("app.db"),
+    HOST: z.string().default('localhost'),
     PORT: z.string().transform(port => Number(port)).default('8000')
 })
