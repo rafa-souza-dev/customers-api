884a84a1d6ffa13c14c9b1f66a7be5cbfa583418
rafa-souza-dev <rafiradeveloper@gmail.com>
2023-02-18 16:17:51 -0300

    refac: created functions to generate next and previous urls

--- src/controllers/FindAndCountAllCustomersController.ts
+++ src/controllers/FindAndCountAllCustomersController.ts
@@ -13,23 +13,36 @@ export class FindAndCountAllCustomersController {
         const { page=1, limit=10 } = findAndCountAllCustomersQueryParamsSchema.parse(req.query)
 
         const { count, results } = await this.findAndCountAllCustomersUseCase.handle(page, limit)
-
-        const resultsCount = page * limit
-        const customersBaseHasNextPage = resultsCount < count
+        
+        const previous = this.previousPageUrlGenerator(page, limit, count)
+        const next = this.nextPageUrlGenerator(page, limit)
 
         const data = {
             count,
             page,
             limit,
-            previous: page > 1 ?
-            `http://localhost:8000/customers?page=${page - 1}&limit=${limit}` :
-            null,
-            next: customersBaseHasNextPage ?
-                `http://localhost:8000/customers?page=${page + 1}&limit=${limit}` :
-                null,
+            previous,
+            next,
             results
         }
 
         return res.status(200).send(data)
+    }
+
+    private nextPageUrlGenerator(page: number, limit: number) {
+        const customersBaseHasPreviousPage = page > 1
+
+        return customersBaseHasPreviousPage ?
+        `http://localhost:8000/customers?page=${page - 1}&limit=${limit}` :
+        null
+    }
+
+    private previousPageUrlGenerator(page: number, limit: number, count: number) {
+        const resultsCount = page * limit
+        const customersBaseHasNextPage = resultsCount < count
+
+        return customersBaseHasNextPage ?
+        `http://localhost:8000/customers?page=${page + 1}&limit=${limit}` :
+        null
     }
 }
